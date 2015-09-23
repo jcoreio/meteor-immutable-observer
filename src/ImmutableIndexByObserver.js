8120028f9885a525f1f49eb494442e97bfdbab9c
Andy Edwards <jedwards@fastmail.com>
2015-09-22 23:33:43 -0500

    add some jsdoc for ImmutableIndexByObserver

--- src/ImmutableIndexByObserver.js
+++ src/ImmutableIndexByObserver.js
@@ -3,6 +3,22 @@ import Immutable from 'immutable';
 import updateDeep from './updateDeep';
 import mergeChanges from './mergeChanges';
 
+/**
+ * This is like `ImmutableMapObserver`, except that instead of using a document's `_id`
+ * for the key in the `Immutable.Map`, it uses a custom key from the `iteratee`.  This
+ * is more efficient than calling `observer.documents().map(...)` every time something
+ * changes, but it is a bit dangerous: when a document is being added, if its key
+ * conflicts with an existing document's key, an `Error` will be thrown; likewise when a
+ * document is being changed, if `iteratee` returns a different key because of the changes
+ * that conflicts with another document's key, an `Error` will be thrown.
+ *
+ * @param{Mongo.Cursor}                 cursor    - the cursor to observe
+ * @param{string|number|array|function} iteratee  - determines the keys of the documents.
+ *    Given `document`, an instance of `Immutable.Map`:
+ *    If `iteratee` is a string or number, uses `document.get(iteratee)` as the key.
+ *    If `iteratee` is an array, uses `document.getIn(iteratee)` as the key.
+ *    If `iteratee` is a function, uses `iteratee(document)` as the key.
+ */
 export default function ImmutableIndexByObserver(cursor, iteratee) {
   if (Tracker.active) {
     throw new Error("This can't be used inside reactive computations; it could cause infinite invalidate loops");
